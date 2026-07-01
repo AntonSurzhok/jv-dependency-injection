8d7c24f5fecc5dd9da647d6a88dd6ead23eb1843
anton <anton.surzhok@gmail.com>
2026-07-01 18:16:49 +0300

    Add changes

--- src/main/java/mate.academy/lib/Injector.java
+++ src/main/java/mate.academy/lib/Injector.java
@@ -1,5 +1,13 @@
 package mate.academy.lib;
 
+import java.lang.reflect.Field;
+import mate.academy.service.FileReaderService;
+import mate.academy.service.ProductParser;
+import mate.academy.service.ProductService;
+import mate.academy.service.impl.FileReaderServiceImpl;
+import mate.academy.service.impl.ProductParserImpl;
+import mate.academy.service.impl.ProductServiceImpl;
+
 public class Injector {
     private static final Injector injector = new Injector();
 
@@ -8,6 +16,47 @@ public class Injector {
     }
 
     public Object getInstance(Class<?> interfaceClazz) {
-        return null;
+        Class<?> implementationClass = findImplementation(interfaceClazz);
+
+        if (!implementationClass.isAnnotationPresent(Component.class)) {
+            throw new RuntimeException(
+                    implementationClass.getSimpleName() + " is not a component");
+        }
+
+        try {
+            Object instance = implementationClass.getDeclaredConstructor().newInstance();
+
+            Field[] fields = implementationClass.getDeclaredFields();
+
+            for (Field field : fields) {
+                if (field.isAnnotationPresent(Inject.class)) {
+                    Object dependency = getInstance(field.getType());
+                    field.setAccessible(true);
+                    field.set(instance, dependency);
+                }
+            }
+
+            return instance;
+        } catch (Exception e) {
+            throw new RuntimeException("Can't create instance of "
+                    + implementationClass.getName(), e);
+        }
+    }
+
+    private Class<?> findImplementation(Class<?> interfaceClazz) {
+        if (interfaceClazz.equals(ProductService.class)) {
+            return ProductServiceImpl.class;
+        }
+
+        if (interfaceClazz.equals(ProductParser.class)) {
+            return ProductParserImpl.class;
+        }
+
+        if (interfaceClazz.equals(FileReaderService.class)) {
+            return FileReaderServiceImpl.class;
+        }
+
+        throw new RuntimeException(
+                "No implementation found for " + interfaceClazz.getName());
     }
 }
